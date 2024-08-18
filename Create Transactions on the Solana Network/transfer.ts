6223dec3ddb59b9f91d43dd76b07d717d378b294
4rjunc <arjunmundamani@gmail.com>
2024-08-18 20:00:43 +0530

    changed labs to name of the section

---
+++ Create Transactions on the Solana Network/transfer.ts
@@ -0,0 +1,22 @@
+import {
+  Connection,
+  Transaction,
+  SystemProgram,
+  sendAndConfirmTransaction,
+  PublicKey,
+} from "@solana/web3.js";
+
+import "dotenv/config";
+
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+
+const suppliedToPubkey = process.argv[2] || null;
+
+if (!suppliedToPubkey) {
+  console.log("Please provide a public key");
+  process.exit(1);
+}
+
+const senderKeypair = getKeypairFromEnvironment("SECRET");
+
+console.log(`senderKeypair: ${senderKeypair}`);
